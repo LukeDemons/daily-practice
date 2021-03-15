a0ef612988e4b55d64f1daa352314cc1578ad949
yanchuang <yanchuang@jd.com>
2021-03-15 16:44:56 +0800

    一高兴，把螺旋矩阵2也给刷了一下，和54题思路一样

---
+++ java/algo/array/ArrayLC59.java
@@ -0,0 +1,55 @@
+package algo.array;
+
+import java.util.Arrays;
+
+/**
+ * https://leetcode-cn.com/problems/spiral-matrix-ii/
+ *
+ * 一高兴，把螺旋矩阵2也给刷了一下，和54题思路一样
+ */
+public class ArrayLC59 {
+
+    public static void main(String[] args) {
+        ArrayLC59 instance = new ArrayLC59();
+
+        int[][] result = instance.generateMatrix(1);
+
+        System.out.println(Arrays.deepToString(result));
+    }
+
+    public int[][] generateMatrix(int n) {
+        int[][] result = new int[n][n];
+        int num = 1;
+
+        int u = 0, d = n - 1, l = 0, r = n - 1;
+
+        while (true) {
+            for (int i = l; i <= r; i++) {
+                result[u][i] = num++;
+            }
+            if (u++ == d) {
+                break;
+            }
+            for (int i = u; i <= d; i++) {
+                result[i][r] = num++;
+            }
+            if (r-- == l) {
+                break;
+            }
+            for (int i = r; i >= l; i--) {
+                result[d][i] = num++;
+            }
+            if (d-- == u) {
+                break;
+            }
+            for (int i = d; i >= u; i--) {
+                result[i][l] = num++;
+            }
+            if (l++ == r) {
+                break;
+            }
+        }
+
+        return result;
+    }
+}

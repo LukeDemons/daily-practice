0d969ec71ccb06f13ac5c81f9c1c5521c5bafd3f
yanchuang <yanchuang@jd.com>
2021-04-14 23:53:39 +0800

    2021-04-14 这题的关键点在于思考出trie树的实现需要创建TrieNode子类（isEnd和TrieNode[]）

---
+++ java/algo/struct/StructLC208.java
@@ -0,0 +1,92 @@
+package algo.struct;
+
+/**
+ * https://leetcode-cn.com/problems/implement-trie-prefix-tree/
+ * <p>
+ * 2021-04-14 每日一题
+ * 这题的关键点在于思考出trie树的实现需要创建TrieNode子类（isEnd和TrieNode[]）
+ */
+public class StructLC208 {
+
+    public static void main(String[] args) {
+        Trie trie = new Trie();
+        trie.insert("apple");
+        System.out.println(trie.search("apple"));   // 返回 True
+        System.out.println(trie.search("app"));     // 返回 False
+        System.out.println(trie.startsWith("app")); // 返回 True
+        trie.insert("app");
+        System.out.println(trie.search("app"));     // 返回 True
+    }
+}
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.insert(word);
+ * boolean param_2 = obj.search(word);
+ * boolean param_3 = obj.startsWith(prefix);
+ */
+class Trie {
+
+    class TireNode {
+        private boolean isEnd;
+        TireNode[] next;
+
+        public TireNode() {
+            isEnd = false;
+            next = new TireNode[26];
+        }
+    }
+
+    private TireNode root;
+
+    /**
+     * Initialize your data structure here.
+     */
+    public Trie() {
+        root = new TireNode();
+    }
+
+    /**
+     * Inserts a word into the trie.
+     */
+    public void insert(String word) {
+        TireNode node = root;
+        for (char c : word.toCharArray()) {
+            if (node.next[c - 'a'] == null) {
+                node.next[c - 'a'] = new TireNode();
+            }
+            // 往后移
+            node = node.next[c - 'a'];
+        }
+        node.isEnd = true;
+    }
+
+    /**
+     * Returns if the word is in the trie.
+     */
+    public boolean search(String word) {
+        TireNode node = root;
+        for (char c : word.toCharArray()) {
+            node = node.next[c - 'a'];
+            if (node == null) {
+                return false;
+            }
+        }
+        return node.isEnd;
+    }
+
+    /**
+     * Returns if there is any word in the trie that starts with the given prefix.
+     */
+    public boolean startsWith(String prefix) {
+        TireNode node = root;
+        for (char c : prefix.toCharArray()) {
+            node = node.next[c - 'a'];
+            if (node == null) {
+                return false;
+            }
+        }
+        return true;
+    }
+}

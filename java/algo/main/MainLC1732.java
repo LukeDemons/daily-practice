7b13ea28fb22521ab12b73a5abe6574448042fe8
yanchuang <yanchuang@jd.com>
2022-11-19 10:03:23 +0800

    2022-11-19 开胃小菜

---
+++ java/algo/main/MainLC1732.java
@@ -0,0 +1,30 @@
+package algo.main;
+
+/**
+ * https://leetcode.cn/problems/find-the-highest-altitude/description/
+ * <p>
+ * 2022-11-19 每日一题
+ * 非常简单
+ */
+public class MainLC1732 {
+
+    public static void main(String[] args) {
+        MainLC1732 instance = new MainLC1732();
+
+        int result = instance.largestAltitude(new int[]{-5, 1, 5, 0, -7});
+
+        System.out.println(result);
+    }
+
+    public int largestAltitude(int[] gain) {
+        int max = 0;
+        int cur = 0;
+        for (int g : gain) {
+            cur += g;
+            if (cur > max) {
+                max = cur;
+            }
+        }
+        return max;
+    }
+}
